fd727fe3ae0d0900dcd22e6476302ef7405866ac
abdulhadi240 <70109681+abdulhadi240@users.noreply.github.com>
2024-05-12 20:44:36 +0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -97,7 +97,10 @@ def predict():
 
 
 if __name__ == '__main__':
+    host = '0.0.0.0'
+    port = 5000
     app.run(debug=True)
-    app.run(host='0.0.0.0', debug=True)
+    app.run(host=host, port=port)
+
 
 
